b4242c8871ce78b2b5d03373039a97e2d3bc1e44
Pylon-hub <onyaricaleb6@gmail.com>
2022-04-16 13:27:46 -0700

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import "./styles.css"
+
+function App()
+
+return (
+  <div className="calculator-grid">
+    <div className="output">
+      <div className="previous-operand">
+        <div className="current-operand">
+          <button className="span-two">AC</button>
+          <button>DEL</button>
+          <button>/</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>*</button>
+          <button>5</button>
+          <button>6</button>
+          <button>+</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+          <button>-</button>
+          <button>.</button>
+          <button>0</button>
+          <button className="span-two">=</button>
+
+
+
+        </div>
+
+
+      </div>
+
+    </div>
+
+  </div>
+)
+
+
+  }
+
+export default App;
+
